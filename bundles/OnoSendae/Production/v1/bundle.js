8a61d0b43cc63a895f1bd4aca98bd10b49973570
Cleber Hensel <cleber.hensel@cwi.com.br>
2025-11-02 21:12:22 -0300

    Release v1 - OnoSendae iOS

---
+++ bundles/OnoSendae/Production/v1/bundle.js
@@ -0,0 +1,10 @@
+console.log('OnoSendae App v1.0.0 - Production Ready!');
+
+export default function App() {
+  return {
+    message: 'Hello from gh-codepush!',
+    version: '1.0.0',
+    timestamp: new Date().toISOString()
+  };
+}
+
